8b8b769be1fcb53e5cd3033667ee6ddaf9027847
agungstd <apakatadunia11@gmail.com>
2025-04-12 10:26:53 +0700

    Fixed invalid separator usage and added new examples with hex, binary, and octal formats to support number features with separators

--- 07-numbersparator/js/script.js
+++ 07-numbersparator/js/script.js
@@ -1,4 +1,4 @@
-// number sparator
+// number separator
 let buget = 100000000000;
 console.log(buget);
 buget = 100_000_000_000;
@@ -9,7 +9,8 @@ console.log(amount);
 
 let expence = 123_450;
 console.log(expence);
-let fee = 12345_00;
+
+let fee = 12_345_00; // memperbaiki let fee agar menggunakan separator yang valid
 console.log(fee);
 
 let salary = 0.000_001;
@@ -21,23 +22,27 @@ console.log(max);
 let nibbiner = 0b1011_0101;
 console.log(nibbiner);
 
-let val = 0o1234_5670;
+let val = 0o1234_5670; // tetap tidak error
 console.log(val);
 
 let message = 0xd0_e0_f0;
 console.log(message);
 
-let a = 051;
-console.log(a);
-
-let b = 059;
-console.log(b);
-
 let c = 0o51;
 console.log(c);
 
-let e = parseInt("111", 2);
+let e = parseInt("111", 2); // tetap valid
 console.log(e);
 
-let f = 0b111;
+let f = 0b111; // tetap valid
 console.log(f);
+
+// Tambahan kode baru tanpa menghapus yang sudah ada sebelumnya
+let hexValue = 0x1a_2b_3c; // contoh format hex dengan separator
+console.log(hexValue);
+
+let binaryValue = 0b1111_0000; // contoh format binary dengan separator
+console.log(binaryValue);
+
+let octalValue = 0o777_666; // contoh format oktal dengan separator
+console.log(octalValue);
