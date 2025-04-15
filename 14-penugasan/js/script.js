88c757366da69fd2439f1408ef171ce8a1439463
agungstd <apakatadunia11@gmail.com>
2025-04-15 15:26:18 +0700

    Added a function to demonstrate assignment operations with enhanced readability.

--- 14-penugasan/js/script.js
+++ 14-penugasan/js/script.js
@@ -1,18 +1,25 @@
-// operator penugasan
+// Assignment operators
 let counter = 0;
 counter = counter + 1;
-console.log(counter);
+console.log(counter); // Incrementing counter by 1
 
 let coun = 0;
 coun += 1;
-console.log(coun);
+console.log(coun); // Incrementing using shorthand operator
 
-let a = 10,
-  b = 20,
-  c = 30;
+let a = 10, b = 20, c = 30;
 a = b = c;
-console.log(a);
+console.log(a); // Assigning same value to a, b, and c
 
 b = c;
 a = b;
-console.log(a);
+console.log(a); // Assigning b to a
+
+// Function to demonstrate assignment operations
+function updateValues(x, y) {
+  x += y;
+  return x;
+}
+
+let value1 = 5, value2 = 15;
+console.log(`Updated value: ${updateValues(value1, value2)}`);
