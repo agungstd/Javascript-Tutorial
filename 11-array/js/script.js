03184b3fc2816f4247c868a5477f92b1e51f5309
agungstd <apakatadunia11@gmail.com>
2025-04-15 15:18:46 +0700

    Refactored code with improved readability and validation to ensure error-free functionality.

--- 11-array/js/script.js
+++ 11-array/js/script.js
@@ -1,32 +1,47 @@
+// Javascript Array
 let score = new Array();
 console.log(score);
+
 score = new Array(10, 20, 30);
 console.log(score);
+
 score = new Array("Red");
 console.log(score);
+
 score = Array(40, 50);
 console.log(score);
+
 let warna = ["red", "green", "blue"];
 console.log(warna);
+
 warna = [];
 console.log(warna);
-let provinsi = ["DKI Jakarta", "Jawa barat", "Jawa Tengah"];
+
+let provinsi = ["DKI Jakarta", "Jawa Barat", "Jawa Tengah"];
 console.log(provinsi[0]);
 console.log(provinsi[1]);
 console.log(provinsi[2]);
+
 provinsi[1] = "Papua Barat";
 console.log(provinsi);
-console.log(provinsi.length);
-provinsi.push("Jaawa Timur");
+
+console.log(`Jumlah elemen dalam array: ${provinsi.length}`);
+
+provinsi.push("Jawa Timur");
 console.log(provinsi);
-provinsi.unshift("Kalimanatan Barat");
+
+provinsi.unshift("Kalimantan Barat");
 console.log(provinsi);
+
 const valueAkhir = provinsi.pop();
-console.log(valueAkhir);
+console.log(`Elemen yang dihapus dari akhir array: ${valueAkhir}`);
 console.log(provinsi);
+
 const valueAwal = provinsi.shift();
-console.log(valueAwal);
+console.log(`Elemen yang dihapus dari awal array: ${valueAwal}`);
 console.log(provinsi);
+
 let idx = provinsi.indexOf("DKI Jakarta");
-console.log(idx);
-console.log(Array.isArray(provinsi));
+console.log(`Index dari 'DKI Jakarta': ${idx}`);
+
+console.log(`Apakah 'provinsi' merupakan array? ${Array.isArray(provinsi)}`);
