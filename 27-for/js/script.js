c96328d285feced58d9198d43727f1b093320912
agungstd <apakatadunia11@gmail.com>
2025-04-22 22:39:23 +0700

    Update For JS

--- 27-for/js/script.js
+++ 27-for/js/script.js
@@ -1,34 +1,47 @@
-// TODO: for loop adalah perulangan dengan 3 parameter
+// TODO: for loop is an iteration statement with three parameters:
 // 1. initializer
 // 2. condition
 // 3. iterator
-//NOTE: perintah dasarnya
-// for (inisiator; condition; iterator){
-//       statement
-//}
+// NOTE: Basic syntax:
+// for (initializer; condition; iterator) {
+//     statement;
+// }
 
 for (let i = 1; i < 5; i++) {
   console.log(i);
 }
 
-// loop tanpa inisiator
+// Loop without initializer
 let j = 1;
 for (; j < 10; j += 2) {
   console.log(j);
 }
-// for loop tanpa kondisi
+
+// For loop without condition
 for (let j = 1; ; j += 2) {
   console.log(j);
   if (j > 10) break;
 }
-// for loop tabpa expresi
+
+// For loop without expression
 j = 1;
 for (;;) {
   if (j > 10) break;
   console.log(j);
   j += 2;
 }
-//for loop tanpa body
+
+// For loop without body
 let sum = 0;
 for (let i = 0; i <= 9; i++, sum += i);
-console.log(sum);
+console.log(`Sum result: ${sum}`);
+
+// Function to dynamically execute a for loop
+function dynamicForLoop(start, condition, step) {
+  for (let i = start; i < condition; i += step) {
+    console.log(i);
+  }
+}
+
+console.log("Dynamic for loop execution:");
+dynamicForLoop(2, 15, 3);
