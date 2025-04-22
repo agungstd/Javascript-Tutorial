f7a4c00f165def29168fcba24835fa59079354a2
agungstd <apakatadunia11@gmail.com>
2025-04-22 22:37:57 +0700

    Enhanced while loop handling with dynamic execution function.

--- 25-while/js/script.js
+++ 25-while/js/script.js
@@ -1,7 +1,7 @@
-// TODO: while loop adalah pernyataan untuk perulangan
-// akan di jalankan terus selama bernilai true
-// while(expresi){
-//     -- statement
+// TODO: while loop is a statement for iteration
+// It will keep running as long as the condition evaluates to true
+// while(expression) { 
+// -- statement 
 // }
 
 let count = 1;
@@ -9,3 +9,14 @@ while (count < 10) {
   console.log(count);
   count += 2;
 }
+
+// Function to execute a while loop dynamically
+function executeWhileLoop(start, condition, step) {
+  while (start < condition) {
+    console.log(start);
+    start += step;
+  }
+}
+
+console.log("Dynamic while loop execution:");
+executeWhileLoop(2, 15, 3);
